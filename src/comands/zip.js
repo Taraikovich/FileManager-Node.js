2e75c6834f7f27e91200906fa5b28b93b2778404
Taraikovich <taraikovi4@gmail.com>
2024-10-14 21:41:15 +0400

    fix: fix archivator

--- src/comands/zip.js
+++ src/comands/zip.js
@@ -1,29 +1,26 @@
 import { createReadStream, createWriteStream } from 'node:fs';
 import { pipeline } from 'node:stream/promises';
 import { resolve, basename, join } from 'node:path';
-import { createGzip, createUnzip } from 'node:zlib';
-
+import { createGzip, createUnzip, createBrotliCompress, createBrotliDecompress } from 'node:zlib';
 
 export const compress = async (arg1, arg2) => {
-    const source = resolve(arg1);
-    const fileName = basename(source);
-    const output = resolve(join(arg2, fileName + '.br'));
+  const source = resolve(arg1);
+  const output = resolve(arg2);
 
-    await pipeline(
-      createReadStream(source),
-      createGzip(),
-      createWriteStream(output)
-    );
-  }
+  await pipeline(
+    createReadStream(source),
+    createBrotliCompress(),
+    createWriteStream(output)
+  );
+};
 
-  export const decompress = async (arg1, arg2) => {
-    const source = resolve(arg1);
-    const fileName = basename(source);
-    const output = resolve(join(arg2, fileName.slice(0, -3)));
+export const decompress = async (arg1, arg2) => {
+  const source = resolve(arg1);
+  const output = resolve(arg2);
 
-    await pipeline(
-      createReadStream(source),
-      createUnzip(),
-      createWriteStream(output)
-    );
-  }
+  await pipeline(
+    createReadStream(source),
+    createBrotliDecompress(),
+    createWriteStream(output)
+  );
+};
